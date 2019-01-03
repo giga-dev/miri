f9df79e3a9ccaec9013477b81fb32d03b5c73e6f
Niv Ingberg <niv@gigaspaces.com>
2019-01-03 10:46:33 +0200

    Added user help on OAuth

--- src/main/java/com/gigaspaces/miri/actions/GithubOAuthAction.java
+++ src/main/java/com/gigaspaces/miri/actions/GithubOAuthAction.java
@@ -13,7 +13,11 @@ public class GithubOAuthAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         String currValue = GitHubAssistant.getOAuthToken();
-        String newValue = Messages.showInputDialog("Enter Github oAuth token", MiriUtils.TITLE, Messages.getQuestionIcon(), currValue, null);
+        String message = "Miri needs an OAuth token to interact with GitHub on your behalf" + System.lineSeparator() +
+                "If you don't have one, you can create it at: https://github.com/settings/tokens" + System.lineSeparator() +
+                "To learn more visit https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/" + System.lineSeparator() +
+                "Enter Github OAuth token:";
+        String newValue = Messages.showInputDialog(message, MiriUtils.TITLE, Messages.getQuestionIcon(), currValue, null);
         if (newValue != null) {
             GitHubAssistant.setOAuthToken(newValue);
             Messages.showInfoMessage(newValue.isEmpty() ? "Cleared" : "Updated", MiriUtils.TITLE);
